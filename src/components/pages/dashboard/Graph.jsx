e3bef44bce347e2f983e64494b70916ab1cd780b
Mangesh2042 <mangesh2042@gmail.com>
2022-10-16 20:14:30 +0530

    graph completed

--- src/components/pages/dashboard/Graph.jsx
+++ src/components/pages/dashboard/Graph.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
-import NavBarAfterLogin from '../navbar/NavBarAfterLogin'
+import { useEffect, useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, AreaChart, Area, BarChart, Bar } from 'recharts';
+import NavBarAfterLogin from '../navbar/NavBarAfterLogin';
+import {url} from "../../../config/url.js"
 
-const Graph = () => {
+
+ 
+
+
+function Graph() {
+
+  const [taskList,setTaskList]=useState([])
+  
+useEffect(()=>{
+  fetch(url+"api/user/taskList")
+  .then(res=>res.json())
+  .then(data=>setTaskList(data.data))
+},[])
+
+
+
   return (
-    <div>
-      <NavBarAfterLogin/>
-    <div>
-        graph
-    </div>
-    </div>
-  )
+    <>
+     <NavBarAfterLogin/>
+      <h1 className="chart-heading">Task chart</h1>
+      <ResponsiveContainer width="100%" aspect={3}>
+        <LineChart data={taskList} width={500} height={300} margin={{ top: 5, right: 300, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" interval={'preserveStartEnd'} tickFormatter={(value) => value + ""} />
+          <YAxis />
+          <Tooltip contentStyle={{ backgroundColor: 'yellow' }} />
+          <Legend />
+
+          <Line type="monotone" dataKey="incomplete" stroke="red" activeDot={{ r: 8 }} />
+
+          <Line type="monotone" dataKey="complete" stroke="green" activeDot={{ r: 8 }} />
+
+        </LineChart>
+      </ResponsiveContainer>
+    </>
+  );
 }
 
-export default Graph
+export default Graph;
